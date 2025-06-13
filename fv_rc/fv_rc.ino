1c99c23723de0d44740fbebb5724bb72c6489331
hensew <wassili.hensew@balluffstm.de>
2025-06-13 13:13:31 +0200

    fv_rc(1st)

---
+++ fv_rc/fv_rc.ino
@@ -0,0 +1,99 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+uint8_t broadcastAddress[] = {0xCC, 0x7B, 0x5C, 0xA7, 0x55, 0x44};
+
+struct nMsg {
+  char cmd;
+  float val;
+};
+
+nMsg myData;
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  if(status == ESP_NOW_SEND_SUCCESS){
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+void Command(char cmd, float val){
+  digitalWrite(LED_BUILTIN, HIGH);
+  myData.cmd = cmd;
+  myData.val = val;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+}
+
+char _rxBuffer[17];
+uint8_t _rxIdx;
+void Terminal(){
+  char c;
+  while(Serial.available() > 0){
+    c = (char)Serial.read();
+    if(c=='\r'){
+      String str = &_rxBuffer[1];
+      Command(_rxBuffer[0], str.toFloat());
+      _rxIdx = 0;
+      _rxBuffer[0] = '\0';
+    }else if (c == '\b' || c == 127) {
+      if (_rxIdx > 0) {
+         _rxIdx--;
+         _rxBuffer[_rxIdx] = '\0';
+      }
+   } else if (isprint(c)) {
+     // Store printable characters in serial receive buffer
+     if (_rxIdx < 16) {
+       _rxBuffer[_rxIdx++] = c;
+       _rxBuffer[_rxIdx] = '\0';
+     }
+    }
+  }
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Transmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  Terminal();
+
+}
