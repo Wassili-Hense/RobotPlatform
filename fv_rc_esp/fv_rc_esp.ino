1c6a93ff8f954d55afcea68d2ae3d4bd01884420
wassili Hense <wassili@hense.email>
2025-07-31 15:21:28 +0200

    fv_rc_stm(ADD) Analog & Digital Inputs, Backlight; fv_rc_esp(ADD) Serial.end

--- fv_rc_esp/fv_rc_esp.ino
+++ fv_rc_esp/fv_rc_esp.ino
@@ -1,46 +1,109 @@
-/*********
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp32-i2c-master-slave-arduino/
-  ESP32 I2C Slave example: https://github.com/espressif/arduino-esp32/blob/master/libraries/Wire/examples/WireSlave/WireSlave.ino
-*********/
-
 #include "Wire.h"
 
+// Joystick
 #define I2C_DEV_ADDR 0x17
+#define ANALOG_DELTA 3
+static uint8_t jrBuff[6];
+static uint8_t jdr = 0;
+static uint16_t jaVal[] = {0, 0, 0};
+static char jaName[] = {'V', 'X', 'Y'};
+static uint8_t jbCnt[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//void onRequest() {
+//  Wire.print(i++);
+//  Wire.print(" Packets.");
+//  Serial.println("onRequest");
+//  Serial.println();
+//}
 
-uint32_t i = 0;
-
-void onRequest() {
-  Wire.print(i++);
-  Wire.print(" Packets.");
-  Serial.println("onRequest");
-  Serial.println();
-}
-
-void onReceive(int len) {
-  Serial.printf("onReceive[%d]: ", len);
-  while (Wire.available()) {
-    Serial.print(Wire.read(), HEX);
-    Serial.print(' ');
+void onJReceive(int len) {
+  if(len==6){
+    for(uint8_t i = 0; i<6; i++){
+      jrBuff[i] = Wire.read();
+    }
+    jdr=1;
   }
-  Serial.println();
 }
+
+int8_t onJAnalog(uint8_t idx){
+  uint16_t val = (jrBuff[idx*2]<<4) | (jrBuff[idx*2+1]&0x0F);
+  int32_t diff = val - jaVal[idx];
+  if(diff>ANALOG_DELTA || diff<-ANALOG_DELTA){
+    jaVal[idx] = val;
+    return 1;
+  }
+  return 0;
+}
+
+int8_t onJDigital(uint8_t idx){
+  uint8_t nv = (jrBuff[((idx&0x0C)>>1)+1]>>((idx&0x03)+4))&1;
+  uint8_t ov = jbCnt[idx]>>7;
+  uint8_t cnt = jbCnt[idx]&3;
+  int8_t r=0;
+  if(nv){
+    if(ov){
+      cnt=3;
+    } else if(cnt<2){
+      cnt++;
+    } else {
+      cnt = 3;
+      ov = 1;
+      r = 1;
+    }
+  } else {
+    if(!ov){
+      cnt = 0;
+    }else if(cnt>1){
+      cnt--;
+    } else {
+      cnt = 0;
+      ov = 0;
+      r = -1;
+    }
+  }
+  jbCnt[idx] = (uint8_t)((ov<<7) | cnt);
+  return r;
+}
+// Serial
+static uint sOpen = false;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Wire.onReceive(onReceive);
-  Wire.onRequest(onRequest);
+  Serial.end();
+  Wire.onReceive(onJReceive);
+  //Wire.onRequest(onRequest);
   Wire.begin((uint8_t)I2C_DEV_ADDR);
-
-/*#if CONFIG_IDF_TARGET_ESP32
-  char message[64];
-  snprintf(message, 64, "%lu Packets.", i++);
-  Wire.slaveWrite((uint8_t *)message, strlen(message));
-  Serial.print('Printing config %lu', i);
-#endif*/
 }
 
 void loop() {
-  
+  if(jdr){
+    jdr = 0;
+    uint8_t i;
+    int8_t r;
+    for(i=0; i<3; i++){
+      r = onJAnalog(i);
+      if(r && sOpen){
+        Serial.print(jaName[i]);
+        Serial.print(": ");
+        Serial.println(jaVal[i]);
+      }
+    }
+    for(i=0; i<12; i++){
+      r = onJDigital(i);
+      if(!r) continue;
+      if(sOpen){
+        Serial.print("K: ");
+        Serial.println((i+1)*r);
+      }
+    }
+  } else {
+    uint8_t usb = (uint8_t)(jbCnt[3]>>7);
+    if(sOpen != usb){
+      sOpen = usb;
+      if(sOpen){
+        Serial.begin(115200);
+      } else {
+        Serial.end();
+      }
+    }
+    delay(1);
+  }
 }
--- fv_rc_stm/fv_rc_stm.ino
+++ fv_rc_stm/fv_rc_stm.ino
@@ -1,90 +1,81 @@
+//CPU - STM32F051C8Tx
+
 #include "Wire.h"
 #define I2C_DEV_ADDR 0x17
+#define DELAY_MS 10
+#define OFF_CNT (1000/DELAY_MS)
+
+uint8_t offCnt = 0;
+uint8_t sndBuff[6];
 
 void setup() {
-  pinMode(PA12, INPUT);         // Button ON
-  pinMode(PA11, OUTPUT);        // Power ON
   pinMode(PA5, INPUT_PULLUP);          // Button 5
   pinMode(PA6, INPUT_PULLUP);          // Button 3
   pinMode(PA7, INPUT_PULLUP);          // Button Down
+  pinMode(PA8, OUTPUT);                // Beep
+  pinMode(PA11, OUTPUT);               // Power ON
+  pinMode(PA12, INPUT);                // Button ON
   pinMode(PB0, INPUT_PULLUP);          // Button UP
+  pinMode(PB1, OUTPUT);                // LCD Backlight
+  Wire.setSCL(PB8);                    // SCL
+  Wire.setSDA(PB9);                    // SDA
   pinMode(PB10, INPUT_PULLUP);         // Button Fire
   pinMode(PC13, INPUT_PULLUP);         // Button Back
   pinMode(PC15, INPUT_PULLUP);         // Button 6
   pinMode(PF1, INPUT_PULLUP);          // Button 4
   pinMode(PF7, INPUT_PULLUP);          // Button Ok
 
-  Wire.setSCL(PB8);
-  Wire.setSDA(PB9);
-
-
-  delay(10);
-  digitalWrite(PA11, HIGH);
   Wire.begin();
+
+  digitalWrite(PA11, HIGH);
+  digitalWrite(PB1, LOW);
+  digitalWrite(PA8, HIGH);
+  delay(10);
+  digitalWrite(PA8, LOW);
   while(!digitalRead(PA12)){
     delay(10);
   }
 }
 
-uint8_t i = 0;
-uint8_t state = 0;
-
-uint8_t Send(uint8_t cmd, uint32_t val){
-  Wire.beginTransmission(I2C_DEV_ADDR);
-  Wire.write(cmd);
-  Wire.write((uint8_t)(val>>24));
-  Wire.write((uint8_t)(val>>16));
-  Wire.write((uint8_t)(val>>8));
-  Wire.write((uint8_t)val);
-  return Wire.endTransmission(true);
-}
 void loop() {
-  delay(20);
-  uint32_t t_u32;
-  uint8_t t_u8;
+  uint32_t tmp;
+  uint8_t b1 = digitalRead(PA12)?0:0x10;
 
-  if(!digitalRead(PA12)){
-    i++;
-  } else if(i>0){
-    i--;
-    if(i>50){
-      digitalWrite(PA11, LOW);
+  if(b1){
+    offCnt++;
+    if(offCnt>OFF_CNT){
+      digitalWrite(PA8, offCnt == OFF_CNT+1?HIGH:LOW);  // Beep
+      digitalWrite(PB1, HIGH);
+      if(offCnt>250){
+        offCnt = OFF_CNT+1;
+      }
+    }
+  } else if(offCnt>0){
+    offCnt--;
+    if(offCnt>=OFF_CNT){
+      digitalWrite(PA11, LOW);  // Power OFF
     }
   }
+  tmp = analogRead(PA3);  // V_USB
+  b1 = b1 | (digitalRead(PB10)?0:0x20) | (tmp>240?0x80:0); 
 
-  switch(state++){
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-      //Send('V', analogRead(PA3));
-      break;
-    case 4:
-      t_u32 = 0;
-      t_u32 |= digitalRead(PA5)?0:0x01;
-      t_u32 |= digitalRead(PA6)?0:0x02;
-      t_u32 |= digitalRead(PA7)?0:0x04;
-      t_u32 |= digitalRead(PB0)?0:0x08;
-      t_u32 |= digitalRead(PB10)?0:0x10;
-      t_u32 |= digitalRead(PC13)?0:0x20;
-      t_u32 |= digitalRead(PC15)?0:0x40;
-      t_u32 |= digitalRead(PF7)?0:0x80;
-      t_u32 |= digitalRead(PF1)?0:0x100;
-      Send('I', t_u32);
-      break;
-    default:
-      state = 0;
-      break;
+  tmp = analogRead(PA0);  // Vcc [250 - 284]
+  sndBuff[0] = (uint8_t)(tmp>>4);
+  sndBuff[1] = (tmp&0x0F) | b1;  // ON, Fire, , USB
+
+  tmp = analogRead(PA1);  // X
+  sndBuff[2] = (uint8_t)(tmp>>4);
+  sndBuff[3] = (tmp&0x0F) | (digitalRead(PB0)?0:0x10) | (digitalRead(PA7)?0:0x20) | (digitalRead(PF7)?0:0x40) | (digitalRead(PC13)?0:0x80); // Up, Down, Ok, Back
+
+  tmp = analogRead(PA2);  // Y
+  sndBuff[4] = (uint8_t)(tmp>>4);
+  sndBuff[5] = (tmp&0x0F) | (digitalRead(PA6)?0:0x10) | (digitalRead(PF1)?0:0x20) | (digitalRead(PA5)?0:0x40) | (digitalRead(PC15)?0:0x80); // B3, B4, B5, B6
+
+  Wire.beginTransmission(I2C_DEV_ADDR);
+  for(uint8_t i = 0; i<6;i++){
+    Wire.write(sndBuff[i]);
   }
-  /*
-  // Read 16 bytes from the slave
-  uint8_t bytesReceived = Wire.requestFrom(I2C_DEV_ADDR, 16);
-  
-  Serial.printf("requestFrom: %u\n", bytesReceived);
-  if ((bool)bytesReceived) {  //If received more than zero bytes
-    uint8_t temp[bytesReceived];
-    Wire.readBytes(temp, bytesReceived);
-    log_print_buf(temp, bytesReceived);
-  }
-  */
+  Wire.endTransmission(true);
+
+  delay(DELAY_MS);
 }
